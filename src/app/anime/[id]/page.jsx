541aafc7f4bbe2884002ff53c9ec4bc101b8b88b
hadiramadhan <hadiramadhan@gmail.com>
2024-05-30 18:47:16 +0700

    update 2

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -15,11 +15,11 @@ const Page = async ({ params: { id }}) =>{
     })
     return (
         <>
-        {/* <div className="pt-4 px-3">
+        <div className="pt-4 px-3">
         <h3 className="text-2xl text-color-primary">{anime.data.title} - {anime.data.year}</h3>
             {!collection && user && <CollectionButton anime_mal_id={id} user_email={user?.email} anime_image={anime.data.images.webp.image_url} anime_title={anime.data.title}/>}
             
-        </div> */}
+        </div>
 
         <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
             <div className="w-36 flex-col justify-center items-center rounded border border-color-primary p-2">
@@ -72,7 +72,7 @@ const Page = async ({ params: { id }}) =>{
             </div>
 
         </div>
-        {/* <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
+        <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
            <Image 
            src={anime.data.images.webp.image_url}
            alt={anime.data.images.jpg.image_url}
@@ -81,8 +81,8 @@ const Page = async ({ params: { id }}) =>{
            className="w-full rounded-object-cover"
            />
            <p className="text-justify text-xl">{anime.data.synopsis}</p>
-        </div> */}
-        {/* <div className="p-4">
+        </div>
+        <div className="p-4">
             <h3 className="text-color-primary text-2xl mb-4">Komentar</h3>
             <CommentBox anime_mal_id={id}/>
 
@@ -93,7 +93,7 @@ const Page = async ({ params: { id }}) =>{
 
         <div>
             <VideoPlayer youtubeId={anime.data.trailer.youtube_id}/>
-        </div> */}
+        </div>
         </>
     )
 }
