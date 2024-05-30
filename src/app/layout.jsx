e910d016cbd943385e3455109a80cb00c2f2ecfd
hadiramadhan <hadiramadhan@gmail.com>
2024-05-30 12:24:09 +0700

    delete gabarito

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,18 @@
 import Navbar from '@/components/Navbar'
 import '@/app/globals.css'
-import { Gabarito } from 'next/font/google'
+// import { Gabarito } from 'next/font/google'
 
-const Gabarito = Gabarito({ subsets: ['latin'] })
+// const gabarito = gabarito({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Nadi Animelist',
-  description: 'Wesbite Anime Hadi',
+  description: 'Wesbite Anime Nadi',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${Gabarito.className} bg-color-dark`} suppressHydrationWarning={true}>
+      <body className={` bg-color-dark`} suppressHydrationWarning={true}>
        <Navbar/>
         {children}</body>
     </html>
